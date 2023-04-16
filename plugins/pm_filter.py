ead1378084ded812c8462bf26334b569fbcb7a1a
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-16 21:27:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1286,6 +1286,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "tamil":
         await query.answer(text=script.TAMIL_TXT, show_alert=True)
+        
+    elif query.data == "imdb1":
+        await query.answer(text=cap, show_alert=True)
 
     elif query.data == "start":
         buttons = [[
@@ -2485,11 +2488,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     await query.answer(MSG_ALRT)
-    
-    elif query.data == "imdb1":
-        await query.answer(text=cap, show_alert=True)
-    try: await query.answer('fdyfubh')
-    except: pass
     
 async def auto_filter(client, msg, spoll=False):
     reqstr1 = msg.from_user.id if msg.from_user else 0
